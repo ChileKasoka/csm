b63b2197355db89e06cc691dcb9251c9e2895934
ChileKasoka <chilekesha.kasoka@cs.unza.zm>
2025-07-11 13:34:43 +0200

    fixed logout func

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,6 +1,9 @@
+import router from '../router'
+
 export function logout() {
   localStorage.removeItem('user');
   localStorage.removeItem('permissions');
   localStorage.removeItem('access_token');
-  window.location.href = '/login';
+  
+  router.push('/login');
 }
